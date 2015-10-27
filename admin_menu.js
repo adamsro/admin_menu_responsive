ccd0434fda5b457507bd09df7335abb65ce51c96
Robert Marshall Adams <rma.kona@gmail.com>
2015-10-26 18:12:28 -0700

    Refactor into new module, admin_menu_responsive

---
+++ admin_menu.js
@@ -0,0 +1,28 @@
+(function($) {
+
+/**
+ * Overrides Drupal.admin.behaviors.hover().
+ */
+Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
+  // Delayed mouseout.
+  $('li.expandable', $adminMenu).hover(
+    function () {
+      // Stop the timer.
+      clearTimeout(this.sfTimer);
+      // Display child lists.
+      $('> ul', this)
+        .css({left: 'auto', display: 'block'})
+        // Immediately hide nephew lists.
+        .parent().siblings('li').children('ul').css({display: 'none'});
+    },
+    function () {
+      // Start the timer.
+      var uls = $('> ul', this);
+      this.sfTimer = setTimeout(function () {
+        uls.css({display: 'none'});
+      }, 400);
+    }
+  );
+};
+
+})(jQuery);
