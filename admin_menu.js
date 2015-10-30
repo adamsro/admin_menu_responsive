54772236e00aec9f99bd2146baeebc5d85f50001
Robert Marshall Adams <rma.kona@gmail.com>
2015-10-29 21:59:39 -0700

    Don't fix position on mobile size

--- admin_menu.js
+++ admin_menu.js
@@ -8,7 +8,7 @@
     // Touch events, if applicable, are triggered before mouseenter and mouseleave.
     $('li.expandable', $adminMenu).on('touchstart', function(event) {
       var uls = $('> ul', this);
-      event.preventDefault(); event.stopPropagation();
+      event.preventDefault(); // event.stopPropagation();
 
       if (event.target.nodeName == 'A') {
         event.target.click(); // Essentially Fastclick
@@ -26,7 +26,7 @@
 
         // If the user touches an element outside of the admin menu then collapse the menu.
         if (isAttached === false) {
-          $(document, context).one('touchstart.admin-menu',function(event){
+          $(document, context).one('touchstart',function(event){
             if (($adminMenu.has(event.target).length === 0)) {
               $('ul ul', $adminMenu).css({display: 'none'});
               isAttached = false;
@@ -59,4 +59,13 @@
     );
 
   };
+
+/**
+ * Apply 'position: fixed' as a class which will only take effect on larger screens.
+ */
+Drupal.admin.behaviors.positionFixed = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.position_fixed) {
+    $adminMenu.addClass('admin-menu-position-fixed');
+  }
+};
 })(jQuery);
