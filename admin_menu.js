4774fd2550b429ccd15b0acaf0174a5fd59361da
Robert Marshall Adams <rma.kona@gmail.com>
2015-10-30 16:38:13 -0700

    Cleanup

--- admin_menu.js
+++ admin_menu.js
@@ -3,55 +3,19 @@
    * Overrides Drupal.admin.behaviors.hover().
    */
   Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
-    var isAttached = false; // only attach document listener once
-
-    // // Touch events, if applicable, are triggered before mouseenter and mouseleave.
-    // $('li.expandable', $adminMenu).on('touchstart', function(event) {
-    //   var uls = $('> ul', this);
-    //   // event.preventDefault(); event.stopPropagation();
-
-    //   // if (event.target.nodeName == 'A') {
-    //   //   event.target.click(); // Essentially Fastclick
-    //   if (uls[0].style.display == 'block') {
-    //     uls.css({display: 'none'});
-    //   } else {
-    //     // Display child lists.
-    //     uls.css({display: 'block'});
-    //     // Immediately hide nephew lists.
-    //     uls.parent().siblings('li').children('ul').css({display: 'none'});
-    //     // Hide child lists
-    //     uls.find('ul').css({display: 'none'});
-    //     // Close other top level menus and their decedents
-    //     uls.parents('ul', $adminMenu).siblings('ul').find('ul').css({display: 'none'});
-
-    //     // If the user touches an element outside of the admin menu then collapse the menu.
-    //     if (isAttached === false) {
-    //       $(document, context).one('touchstart',function(event){
-    //         if (($adminMenu.has(event.target).length === 0)) {
-    //           $('ul ul', $adminMenu).css({display: 'none'});
-    //           isAttached = false;
-    //         }
-    //       });
-    //       isAttached = true;
-    //     }
-    //   }
-    // });
     var allowClick = true;
 
-    $('li.expandable .handle', $adminMenu).on('click', function(event) {
+    // Clicks open and close sections as well as hover.
+    $('li.expandable span', $adminMenu).on('click', function(event) {
       if (allowClick === true) {
         var uls = $('~ ul', this);
         if (uls[0].style.display == 'block') {
-          // uls.parent().trigger('mouseleave');
-        if (allowClick === true) {
           uls.css({display: 'none'});
-        }
         } else {
           uls.parent().trigger('mouseenter');
         }
       }
     });
-
 
     // Delayed mouseout.
     $('li.expandable', $adminMenu).hover(
@@ -75,7 +39,6 @@
           setTimeout(function() {
             allowClick = true;
           }, 0);
-          // return false;
         }
       },
       function (event) {
