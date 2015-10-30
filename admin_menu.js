68b0fb7644b08834ab9e2b5a33d06ff843157bb2
Robert Marshall Adams <rma.kona@gmail.com>
2015-10-30 13:36:47 -0700

    It actually works I think. Dont touch anything.

--- admin_menu.js
+++ admin_menu.js
@@ -5,34 +5,49 @@
   Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
     var isAttached = false; // only attach document listener once
 
-    // Touch events, if applicable, are triggered before mouseenter and mouseleave.
-    $('li.expandable', $adminMenu).on('touchstart', function(event) {
-      var uls = $('> ul', this);
-      event.preventDefault(); // event.stopPropagation();
+    // // Touch events, if applicable, are triggered before mouseenter and mouseleave.
+    // $('li.expandable', $adminMenu).on('touchstart', function(event) {
+    //   var uls = $('> ul', this);
+    //   // event.preventDefault(); event.stopPropagation();
 
-      if (event.target.nodeName == 'A') {
-        event.target.click(); // Essentially Fastclick
-      } else if (event.target.nodeName == "SPAN" && uls[0].style.display == 'block') {
-        uls.css({display: 'none'});
-      } else if (event.target.nodeName == "SPAN") {
-        // Display child lists.
-        uls.css({display: 'block'});
-        // Immediately hide nephew lists.
-        uls.parent().siblings('li').children('ul').css({display: 'none'});
-        // Hide child lists
-        uls.find('ul').css({display: 'none'});
-        // Close other top level menus and their decedents
-        uls.parents('ul', $adminMenu).siblings('ul').find('ul').css({display: 'none'});
+    //   // if (event.target.nodeName == 'A') {
+    //   //   event.target.click(); // Essentially Fastclick
+    //   if (uls[0].style.display == 'block') {
+    //     uls.css({display: 'none'});
+    //   } else {
+    //     // Display child lists.
+    //     uls.css({display: 'block'});
+    //     // Immediately hide nephew lists.
+    //     uls.parent().siblings('li').children('ul').css({display: 'none'});
+    //     // Hide child lists
+    //     uls.find('ul').css({display: 'none'});
+    //     // Close other top level menus and their decedents
+    //     uls.parents('ul', $adminMenu).siblings('ul').find('ul').css({display: 'none'});
 
-        // If the user touches an element outside of the admin menu then collapse the menu.
-        if (isAttached === false) {
-          $(document, context).one('touchstart',function(event){
-            if (($adminMenu.has(event.target).length === 0)) {
-              $('ul ul', $adminMenu).css({display: 'none'});
-              isAttached = false;
-            }
-          });
-          isAttached = true;
+    //     // If the user touches an element outside of the admin menu then collapse the menu.
+    //     if (isAttached === false) {
+    //       $(document, context).one('touchstart',function(event){
+    //         if (($adminMenu.has(event.target).length === 0)) {
+    //           $('ul ul', $adminMenu).css({display: 'none'});
+    //           isAttached = false;
+    //         }
+    //       });
+    //       isAttached = true;
+    //     }
+    //   }
+    // });
+    var allowClick = true;
+
+    $('li.expandable .handle', $adminMenu).on('click', function(event) {
+      if (allowClick === true) {
+        var uls = $('~ ul', this);
+        if (uls[0].style.display == 'block') {
+          // uls.parent().trigger('mouseleave');
+        if (allowClick === true) {
+          uls.css({display: 'none'});
+        }
+        } else {
+          uls.parent().trigger('mouseenter');
         }
       }
     });
@@ -41,20 +56,38 @@
     // Delayed mouseout.
     $('li.expandable', $adminMenu).hover(
       function (event) {
-        // Stop the timer.
-        clearTimeout(this.sfTimer);
-        // Display child lists.
-        $('> ul', this)
-          .css({left: 'auto', display: 'block'})
+        if (allowClick === true) {
+          // Stop the timer.
+          clearTimeout(this.sfTimer);
+          // Display child lists.
+          var uls = $('> ul', this);
+          uls.css({left: 'auto', display: 'block'});
           // Immediately hide nephew lists.
-          .parent().siblings('li').children('ul').css({display: 'none'});
+          uls.parent().siblings('li').children('ul').css({display: 'none'});
+         // Sometimes child lists arent hidden correctly.
+          uls.find('ul').css({display: 'none'});
+          // Close other top level menus and their decedents
+          // uls.parents('ul', $adminMenu).siblings('ul').find('ul').css({display: 'none'});
+          // Hide child lists
+          uls.find('ul').css({display: 'none'});
+
+          allowClick = false;
+          setTimeout(function() {
+            allowClick = true;
+          }, 0);
+          // return false;
+        }
       },
-      function () {
+      function (event) {
         // Start the timer.
+        if (allowClick === true) {
         var uls = $('> ul', this);
-        this.sfTimer = setTimeout(function () {
           uls.css({display: 'none'});
-        }, 400);
+        }
+        // this.sfTimer = setTimeout(function () {
+        //   uls.css({display: 'none'});
+        // }, 400);
+        // return false;
       }
     );
 
