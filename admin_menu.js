c08692b3db8e6000048f7bd453966c9d517223ef
Robert Marshall Adams <rma.kona@gmail.com>
2015-10-29 21:25:28 -0700

    Add listener to document on first menu open. Remove admin-menu-wrapper

--- admin_menu.js
+++ admin_menu.js
@@ -3,6 +3,7 @@
    * Overrides Drupal.admin.behaviors.hover().
    */
   Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
+    var isAttached = false; // only attach document listener once
 
     // Touch events, if applicable, are triggered before mouseenter and mouseleave.
     $('li.expandable', $adminMenu).on('touchstart', function(event) {
@@ -13,26 +14,29 @@
         event.target.click(); // Essentially Fastclick
       } else if (event.target.nodeName == "SPAN" && uls[0].style.display == 'block') {
         uls.css({display: 'none'});
-        $(this).removeClass('active');
       } else if (event.target.nodeName == "SPAN") {
         // Display child lists.
         uls.css({display: 'block'});
-        $(this).addClass('active');
         // Immediately hide nephew lists.
         uls.parent().siblings('li').children('ul').css({display: 'none'});
         // Hide child lists
         uls.find('ul').css({display: 'none'});
         // Close other top level menus and their decedents
         uls.parents('ul', $adminMenu).siblings('ul').find('ul').css({display: 'none'});
+
+        // If the user touches an element outside of the admin menu then collapse the menu.
+        if (isAttached === false) {
+          $(document, context).one('touchstart.admin-menu',function(event){
+            if (($adminMenu.has(event.target).length === 0)) {
+              $('ul ul', $adminMenu).css({display: 'none'});
+              isAttached = false;
+            }
+          });
+          isAttached = true;
+        }
       }
     });
 
-    // If the user touches an element outside of the admin menu then collapse the menu.
-    // $(document).on('touchstart.admin-menu',function(event){
-    //   if (($adminMenu.children().has(event.target).length === 0)) {
-    //     $('ul ul', $adminMenu).css({display: 'none'});
-    //   };
-    // });
 
     // Delayed mouseout.
     $('li.expandable', $adminMenu).hover(
@@ -44,14 +48,12 @@
           .css({left: 'auto', display: 'block'})
           // Immediately hide nephew lists.
           .parent().siblings('li').children('ul').css({display: 'none'});
-        $(this).addClass('active');
       },
       function () {
         // Start the timer.
         var uls = $('> ul', this);
         this.sfTimer = setTimeout(function () {
           uls.css({display: 'none'});
-          $(this).removeClass('active');
         }, 400);
       }
     );
